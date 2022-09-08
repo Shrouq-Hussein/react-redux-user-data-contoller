5edf3df9249d86237d978d43036515fbeb515016
Shrouk Hussein <shrouq.hussein.elsayed@gmail.com>
2022-09-08 13:44:28 +0200

    .

--- app/src/components/users/users.js
+++ app/src/components/users/users.js
@@ -1,13 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./users.css"
-import { fetchUsers, createUser ,updateUser,deleteUser} from "../../store/users/users.actions"
+import { fetchUsers, createUser, updateUser, deleteUser } from "../../store/users/users.actions"
+import UserForm from "../form/form"
+import { Button } from "reactstrap"
+
+import MaterialTable from "material-table";
+import tableIcons from "./MaterialTableIcons";
+
 export default function Users() {
+    const [modal, setModal] = useState(false);
+    const [userId, setUserId] = useState("");
+
+    const toggle = () => setModal(!modal);
+
+
     const dispatch = useDispatch();
     const usersList = useSelector(({ users }) => users.users);
     const isLoading = useSelector(({ users }) => users.isLoading);
     const errorMessage = useSelector(({ users }) => users.errorMessage);
-
+    const columns = [
+        { 
+            title: "Pic",
+            field: "picture" ,
+            render: (rowData) => <img src={rowData.picture} style={{ width: 40, borderRadius: "50%" }} />,
+        },
+        { title: "Title", field: "title" },
+        { title: "First Name", field: "firstName" },
+        { title: "Last Name", field: "lastName" },
+  
+    ]
     useEffect(() => {
         dispatch(fetchUsers())
         console.log(usersList)
@@ -17,7 +39,7 @@ export default function Users() {
 
 
     return (
-        <>
+        <div className="p-2">
             {
                 errorMessage ?
                     errorMessage :
@@ -26,32 +48,35 @@ export default function Users() {
                         :
                         usersList.length !== 0 &&
                         <div className="usersContainer">
-                              <button onClick={() => {
-                                dispatch(createUser({
-                                    firstName: "new",
-                                    lastName: "user",
-                                    email: "email@ff.com",
-                                    title: "mr",
-                                    picture: "",
-                                }))
-                            }}>add user</button>
-                            {
+
+                            <div className="header my-3">
+                                <h1>User Management</h1>
+                                <Button color="success" onClick={() => { toggle(); setUserId("") }}>
+                                    + Add New
+                                </Button>
+                            </div>
+                            <UserForm modal={modal} toggle={toggle} userid={userId} />
+
+                            <MaterialTable title="" columns={columns} data={usersList} icons={tableIcons} />
+                            {/* {
                                 usersList.map((user) =>
                                 (
                                     <div key={user.id} className="userRow">
-                                    <h1  onClick={() => { dispatch(updateUser(user.id, {firstName:"Sarsoooooor",})) }} >
-                                       firstName:  { user.firstName} | lastName: {user.lastName} | title: {user.title}
-                                    </h1>
-                                    <h1 onClick={()=>{dispatch(deleteUser(user.id))}} className="delete">delete</h1>
+                                        {/* <h1 onClick={() => { dispatch(updateUser(user.id, { firstName: "Sarsoooooor", })) }} > *
+                                        <h1 onClick={()=>{ toggle() ; setUserId(user.id)}} >
+
+                                            firstName:  {user.firstName} | lastName: {user.lastName} | title: {user.title}
+                                        </h1>
+                                        <h1 onClick={() => { dispatch(deleteUser(user.id)) }} className="delete">delete</h1>
                                     </div>
-                                    
+
 
                                 )
                                 )
-                            }
-                          
+                            } */}
+
                         </div>
             }
-        </>
+        </div>
     )
 }
